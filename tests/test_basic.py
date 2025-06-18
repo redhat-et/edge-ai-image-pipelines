4f1560ce732c02fe5b223b1f3f2aac18f79c9955
Nick Cao <nickcao@nichi.co>
2025-06-18 14:18:06 -0400

    Test base image

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,19 +1,12 @@
 import sys
 
-from opendal import Operator
-
 from jumpstarter_testing.pytest import JumpstarterTest
 
 
 class TestQemu(JumpstarterTest):
     def test_driver_qemu(tmp_path, client):
         with client.log_stream():
-            client.storage.flash(
-                "pub/fedora/linux/releases/41/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2",
-                operator=Operator(
-                    "http", endpoint="https://download.fedoraproject.org"
-                ),
-            )
+            client.storage.flash("./image.raw")
 
             client.power.cycle()
 
