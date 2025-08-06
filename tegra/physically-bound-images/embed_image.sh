51c3fcb00a3fbf96c1951904debcb29954597b54
vraiti <vraiti@redhat.com>
2025-08-06 09:22:08 -0400

    More copy embed images

--- tegra/physically-bound-images/embed_image.sh
+++ tegra/physically-bound-images/embed_image.sh
@@ -3,13 +3,26 @@
 set -euxo pipefail
 
 image=$1
-repo="${image%%:*}"
-spec="${image#*:}"
-tag="${spec%%@*}"
-sha="${spec#*@}"
-
 additional_copy_args=${2:-""}
 
+fsha=$(echo "$image" | sha256sum | awk '{ print $1 }')
+
+src=$image
+dst=$image
+
+# Skopeo does not support references of type $REPO:$TAG@sha256:$SHA, so if
+# references are given in such a format, exclude the tag from the skopeo copy
+if [[ $image =~ .*:.*@sha256:.* ]]; then
+	repo="${image%%:*}"
+	spec="${image#*:}"
+	tag="${spec%%@*}"
+	sha="${spec#*@}"
+
+	src=$repo@$sha
+	dst=$repo:$tag
+fi
+
+
 mkdir -p /usr/lib/containers-image-cache
-skopeo copy $additional_copy_args --preserve-digests docker://$repo@$sha dir:/usr/lib/containers-image-cache/$sha
-echo "$repo:$tag,$sha" >> /usr/lib/containers-image-cache/mapping.txt
+skopeo copy $additional_copy_args --preserve-digests docker://$src dir:/usr/lib/containers-image-cache/$fsha
+echo "$dst,$fsha" >> /usr/lib/containers-image-cache/mapping.txt
